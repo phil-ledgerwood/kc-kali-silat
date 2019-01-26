3d5ce3924036b83a05e3d70890bdbdde58385971
phil-ledgerwood <pledgerwood@gmail.com>
2019-01-26 10:27:49 -0600

    Metadata images for more devices

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -24,17 +24,21 @@ const TemplateWrapper = ({ children }) => (
           <title>{data.site.siteMetadata.title}</title>
           <meta name="description" content={data.site.siteMetadata.description} />
           
-          <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
-	        <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
-	        <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/img/metadata-images/apple-touch-icon.png" />
+	        <link rel="icon" type="image/png" href="/img/metadata-images/favicon-32x32.png" sizes="32x32" />
+	        <link rel="icon" type="image/png" href="/img/metadata-images/favicon-16x16.png" sizes="16x16" />
+          <link rel="icon" type="image/png" href="/img/metadata-images/android-chrome-192x192.png" sizes="192x192" />
+          <link rel="icon" type="image/png" href="/img/metadata-images/android-chrome-512x512.png" sizes="512x512" />
 	
-	        <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
+	        <link rel="mask-icon" href="/img/metadata-images/safari-pinned-tab.svg" color="#ff4400" />
 	        <meta name="theme-color" content="#fff" />
+          <meta name="msapplication-TileColor" content="#efddb2" />
+          <meta name="msapplication-TileImage" content="/img/metadata-images/mstile-150x150.png" />
 
 	        <meta property="og:type" content="business.business" />
           <meta property="og:title" content={data.site.siteMetadata.title} />
           <meta property="og:url" content="/" />
-          <meta property="og:image" content="/img/og-image.png" />
+          <meta property="og:image" content="/img/metadata-images/og-image.png" />
         </Helmet>
         <Navbar />
         <div>{children}</div>
